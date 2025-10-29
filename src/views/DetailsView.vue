4219e6321cc275221b9fe029ad0b7d9d8944b359
Jaron Zijlstra <support@jaronline.dev>
2025-10-29 17:12:19 +0100

    feat: add details page

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import type {TodoItemInfo} from "@/TodoItemInfo.ts";
+import {useRoute, onBeforeRouteUpdate} from "vue-router";
+import {ref} from "vue";
 
+const route = useRoute();
+
+const items: TodoItemInfo[] = [
+  {
+    id: 1,
+    name: "Buy groceries",
+    dueDate: new Date('2024-07-01'),
+  },
+  {
+    id: 2,
+    name: "Walk the dog",
+    dueDate: new Date('2024-07-02')
+  }
+];
+
+const todoItem = ref<TodoItemInfo | null>(null);
+
+function setTodoItem(id: number) {
+  todoItem.value = items.find(item => item.id === id) || null;
+}
+
+onBeforeRouteUpdate((to, from) => {
+  setTodoItem(Number(to.params.id));
+});
+
+setTodoItem(Number(route.params.id));
 </script>
 
 <template>
-<p>Details view {{ $route.params.id }}</p>
+  <h1>{{todoItem?.name}}</h1>
+  <p>Due by: {{ todoItem?.dueDate?.toDateString() }}</p>
 </template>
 
 <style scoped>
