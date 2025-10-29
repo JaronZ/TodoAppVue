4c126778c9d176a28fd06c7db0e8f0e97da8aaa1
Jaron Zijlstra <support@jaronline.dev>
2025-10-29 13:20:56 +0100

    feat: add simple header with nav

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <a href="/">Home</a>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: lightgray;
+  padding: 20px;
+}
+</style>
